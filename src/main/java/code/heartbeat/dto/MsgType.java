3e6fddbeb6f95a5a07817a769401f53f955df65c
18600909436@163.com <wcx3412232355>
2018-05-31 14:24:38 +0800

    first commit

--- src/main/java/code/heartbeat/dto/MsgType.java
+++
@@ -1,8 +0,0 @@
-package code.heartbeat.dto;
-
-/**
- *
- */
-public enum  MsgType {
-    PING,ASK,REPLY,LOGIN
-}
--- src/main/java/code/heartbeat/dto/ReplyBody.java
+++
@@ -1,7 +0,0 @@
-package code.heartbeat.dto;
-
-import java.io.Serializable;
-
-public class ReplyBody implements Serializable {
-    private static final long serialVersionUID = 1L;
-}
